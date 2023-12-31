0d7a673f6179fdff01e6a5b5f1a702d8946cd386
Suraj Prajapat <sprajapat770@gmail.com>
2023-12-31 22:27:33 +0530

    graphql mutation

--- index.js
+++ index.js
@@ -27,12 +27,63 @@ const resolvers = {
         author(_, args, context) {
             return db.authors.find(author => author.id === args.id);
         }
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter(review => review.game_id === parent.id);
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter(review => review.author_id === parent.id);
+        }
+    },
+    Review: {
+        game(parent) {
+            return db.games.find(game => game.id === parent.game_id);
+        },
+        author(parent) {
+            return db.authors.find(author => author.id === parent.author_id);
+        }
+    },
+    Mutation: {
+        deleteGame(_, args) {
+            db.games = db.games.filter(game => game.id !== args.id);
+            return db.games;
+        },
+        addGame(_, args) {
+            let game = {
+                ...args.game,
+                id: Math.floor(Math.random() * 10000).toString()
+            }
+
+            db.games.push(game);
+            return game;
+        },
+        updateGame(_, args) {
+            db.games = db.games.map(game => {
+                if(game.id === args.id)
+                    return {...game, ...args.edits};
+                return game; 
+            });   
+            return db.games.find(game => game.id === args.id);
+        }
     }
 }
 
 /*
 games {
     title
+}
+
+query GameQuery($id: ID!) {
+    game(id: $id) {
+        title
+        reviews {
+            rating
+            content
+        }
+    }
 }
 */
 
